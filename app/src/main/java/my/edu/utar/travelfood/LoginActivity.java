47ceb1ed326112708ac4b5368df7093970e03ee3
oks0114 <ongkarseng14@gmail.com>
2022-04-08 15:28:28 +0800

    added register

---
+++ app/src/main/java/my/edu/utar/travelfood/LoginActivity.java
@@ -0,0 +1,125 @@
+package my.edu.utar.travelfood;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.os.Handler;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class LoginActivity extends AppCompatActivity {
+
+    EditText emailEditText;
+    EditText passwordEditText;
+    Button loginButton;
+    Button registerButton;
+    ProgressBar progressBar;
+    private FirebaseAuth mAuth = FirebaseAuth.getInstance();;
+    SharedPreferences sharedPreferences;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Auto login
+//        sharedPreferences = this.getSharedPreferences("my.edu.utar.travelfood", Context.MODE_PRIVATE);
+//        if (sharedPreferences.getBoolean("loginState", false)) {
+//            Intent intent = new Intent(this, MainActivity.class);
+//            startActivity(intent);
+//        }
+
+        setContentView(R.layout.activity_login);
+
+        emailEditText = findViewById(R.id.emailEditText);
+        passwordEditText = findViewById(R.id.passwordEditText);
+        loginButton = findViewById(R.id.loginButton);
+        registerButton = findViewById(R.id.registerButton);
+        progressBar = findViewById(R.id.progressBar2);
+
+        // Add listeners
+        emailEditText.addTextChangedListener(createTextWatcher(emailEditText));
+        passwordEditText.addTextChangedListener(createTextWatcher(passwordEditText));
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    public void onLoginClicked(View view) {
+        String username = emailEditText.getText().toString();
+        String password = passwordEditText.getText().toString();
+
+        if (username.isEmpty()) { // check if username is empty
+            emailEditText.setError("Email must not be empty!");
+        } else if (password.isEmpty()) { // check if password is empty
+            passwordEditText.setError("Password must not be empty!");
+        }  else { // check if username and password exists in firebase
+            mAuth.signInWithEmailAndPassword(emailEditText.getText().toString(), passwordEditText.getText().toString())
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) { // login successful
+                                performLogin();
+                            } else { // login failed
+                                Toast.makeText(LoginActivity.this, "Login failed. Try again!", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+        }
+    }
+
+    // Listener
+    private TextWatcher createTextWatcher(EditText editText) {
+        return new TextWatcher() {
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                editText.setError(null);
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+            }
+        };
+    }
+
+    public void performLogin() {
+        //sharedPreferences.edit().putBoolean("loginState", true).apply();
+
+        emailEditText.setEnabled(false);
+        passwordEditText.setEnabled(false);
+        loginButton.setVisibility(View.INVISIBLE);
+        progressBar.setVisibility(View.VISIBLE);
+
+        // Show progress bar for 2 seconds, then go to main activity
+        Handler handler = new Handler();
+        handler.postDelayed(() -> {
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        }, 2000);
+    }
+}
---
+++ app/src/main/java/my/edu/utar/travelfood/MainActivity.java
@@ -0,0 +1,94 @@
+package my.edu.utar.travelfood;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.renderscript.ScriptGroup;
+import android.util.Log;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class MainActivity extends AppCompatActivity {
+    public class DownloadTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... urls) {
+            String result = "";
+            URL url;
+            HttpURLConnection urlConnection = null;
+
+            try {
+                url = new URL(urls[0]);
+                urlConnection = (HttpURLConnection) url.openConnection();
+                InputStream in = urlConnection.getInputStream();
+                InputStreamReader reader = new InputStreamReader(in);
+
+                int data = reader.read();
+
+                while (data != -1) {
+                    char current = (char) data;
+                    result += current;
+                    data = reader.read();
+                }
+                return result;
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+
+            try {
+                JSONObject jsonObject = new JSONObject(s);
+                String mealInfo = jsonObject.getString("meals");
+
+                JSONArray arr = new JSONArray(mealInfo);
+
+                for (int i=0; i<arr.length(); i++) {
+                    JSONObject jsonPart = arr.getJSONObject(i);
+                    String mealName = jsonPart.getString("strMeal");
+                    String instruction = jsonPart.getString("strInstructions");
+                    Log.i("Meal Name", mealName);
+                    Log.i("Instruction", instruction);
+                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        DownloadTask task = new DownloadTask();
+        task.execute("https://www.themealdb.com/api/json/v1/1/search.php?s=Apam%20balik");
+    }
+}
